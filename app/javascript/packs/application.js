c2d2ffb7747dbd2914ef17b71ce50a87d60158a0
Rakib Mondal <rakibmondal2155@gmail.com>
2026-05-27 14:19:38 +0530

    Added React.js to the project

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,14 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails";
-import "./controllers";
 import "../stylesheets/application.scss";
+import ReactRailsUJS from "react_ujs";
+import App from "../src/App";
 
 import { setAuthHeaders } from "apis/axios";
 import { initializeLogger } from "common/logger";
 
 initializeLogger();
 setAuthHeaders();
+
+const componentsContext = { App };
+ReactRailsUJS.getConstructor = (name) => {
+  return componentsContext[name];
+};
--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,7 @@ module Granite
     config.generators do |g|
       g.test_framework :test_unit, fixture: false
     end
+
+    config.assets.pipeline = :sprockets
   end
 end
